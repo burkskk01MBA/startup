231d4a39f3e034bfd5e299c6d1b81ff1a01fff4e
burkskk01MBA <128370379+burkskk01MBA@users.noreply.github.com>
2023-03-20 05:59:35 -0700

    Update appraisal_app.py

--- appraisal_app.py
+++ appraisal_app.py
@@ -1,85 +1,41 @@
 import streamlit as st
 import requests
-import base64
 
-# Function to fetch data from House Canary API
 def get_house_data(address, zipcode, api_key, api_secret):
-    url = f"https://api.housecanary.com/v2/property/details?address={address}&zipcode={zipcode}"
-    headers = {
-        "Authorization": "Basic " + base64.b64encode(f"{api_key}:{api_secret}".encode("ascii")).decode("ascii")
+    endpoint = "https://api.housecanary.com/v2/property/details"
+    payload = {
+        "address": address,
+        "zipcode": zipcode
     }
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()["property/details"]["result"]
-    else:
-        st.error("Failed to fetch data. Please try again.")
-        return None
-
-# Function to display data
-def display_house_data(house_data):
-    st.header("Property Details")
-
-    try:
-        # Display address
-        st.subheader("Address")
-        property_data = house_data['property']
-        address_data = house_data['address_info']
-        st.markdown(f"{address_data['address']}, {address_data['city']}, {address_data['state']} {address_data['zipcode']}")
-
-        # Display property details in columns
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.subheader("Property Type")
-            st.write(property_data['property_type'])
-
-            st.subheader("Bedrooms")
-            st.write(property_data.get('number_of_bedrooms', 'N/A'))
-
-            st.subheader("Bathrooms")
-            st.write(property_data.get('total_bath_count', 'N/A'))
-
-            st.subheader("Year Built")
-            st.write(property_data.get('year_built', 'N/A'))
-
-        with col2:
-            st.subheader("Living Area (sq.ft)")
-            st.write(property_data.get('building_area_sq_ft', 'N/A'))
-
-            st.subheader("Lot Area (sq.ft)")
-            st.write(property_data.get('site_area_acres', 'N/A'))
-
-            st.subheader("Assessed Value")
-            st.write(property_data.get('assessed_value', 'N/A'))
-
-    except KeyError as e:
-        st.error(f"Failed to display data. Missing field: {e}")
+    headers = {
+        "X-API-KEY": api_key,
+        "X-API-SECRET": api_secret,
+        "Content-Type": "application/json"
+    }
+    response = requests.post(endpoint, json=payload, headers=headers)
+    return response.json()["property/details"]["result"]
 
 def app():
-    st.title("House Canary Property Details")
+    st.title("Real Estate Appraisal")
 
-    # Add a sidebar for user input
-    st.sidebar.header("Input Property Information")
+    st.markdown("""
+    This web app fetches a real estate appraisal using HouseCanary's API.
+    """)
 
-    address = st.sidebar.text_input("Address", "123 Main St")
-    zipcode = st.sidebar.text_input("Zipcode", "12345")
+    address = st.text_input("Enter the address of the property")
+    zipcode = st.text_input("Enter the zipcode of the property")
 
-    api_key = "test_AQ5GSA1A96947PDOJJXE"
-    api_secret = "zxkVIQ9O3g3YkFVEZTOIs4TvCmb00kd3"
-
-    fetch_button = st.sidebar.button("Fetch Property Details")
-
-    if fetch_button:
-        # Fetch data
+    if st.button("Get appraisal"):
+        api_key = "test_AQ5GSA1A96947PDOJJXE"
+        api_secret = "zxkVIQ9O3g3YkFVEZTOIs4TvCmb00kd3"
         house_data = get_house_data(address, zipcode, api_key, api_secret)
 
-        if house_data:
-            display_house_data(house_data)
-
-    # Add a footer
-    st.markdown("---")
-    st.markdown("This web app is using data from the House Canary API. For more information, please visit their [website](https://www.housecanary.com/).")
+        st.write(f"Property value: {house_data['avm']['value']}")
+        st.write(f"Last sale date: {house_data['lastSaleDate']}")
+        st.write(f"Last sale amount: {house_data['lastSaleAmount']}")
+        st.write(f"Year built: {house_data['yearBuilt']}")
+        st.write(f"Number of bedrooms: {house_data['bedrooms']}")
+        st.write(f"Number of bathrooms: {house_data['bathrooms']}")
 
 if __name__ == "__main__":
     app()
