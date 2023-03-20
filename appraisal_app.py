45b679f1eec7f5d1c50a29f55a385019991bfd9c
burkskk01MBA <128370379+burkskk01MBA@users.noreply.github.com>
2023-03-20 05:18:27 -0700

    Update appraisal_app.py

--- appraisal_app.py
+++ appraisal_app.py
@@ -27,17 +27,14 @@ def get_house_data(address, zipcode, api_key, api_secret):
 
 # Define function to display house data
 def display_house_data(house_data):
-    property_data = house_data['property/details']['result']['property']
-    assessment_data = house_data['property/details']['result']['assessment']
-    
     st.subheader("Property Details")
-    st.markdown(f"**Address**: {property_data['address']['line1']}, {property_data['address']['city']}, {property_data['address']['state']} {property_data['address']['zipcode']}")
-    st.write(f"**Built Year**: {property_data['year_built']}")
-    st.write(f"**Building Area**: {property_data['building_area_sq_ft']} sq. ft.")
-    st.write(f"**Site Area**: {property_data['site_area_acres']} acres")
-    st.write(f"**Building Quality Score**: {property_data['building_quality_score']}")
-    st.write(f"**Assessed Value**: {assessment_data['total_assessed_value']}")
-    st.write(f"**Tax Amount**: {assessment_data['tax_amount']}")
+    st.markdown(f"**Address**: {house_data['property']['address']['line1']}, {house_data['property']['address']['city']}, {house_data['property']['address']['state']} {house_data['property']['address']['zipcode']}")
+    st.write(f"**Built Year**: {house_data['property']['year_built']}")
+    st.write(f"**Building Area**: {house_data['property']['building_area_sq_ft']} sq. ft.")
+    st.write(f"**Site Area**: {house_data['property']['site_area_acres']} acres")
+    st.write(f"**Building Quality Score**: {house_data['property']['building_quality_score']}")
+    st.write(f"**Assessed Value**: {house_data['assessment']['total_assessed_value']}")
+    st.write(f"**Tax Amount**: {house_data['assessment']['tax_amount']}")
     
 # Define the Streamlit app
 def app():
