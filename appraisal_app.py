49712a7fbbb9850e8064739777f95cbe9364e56c
burkskk01MBA <128370379+burkskk01MBA@users.noreply.github.com>
2023-03-20 04:39:08 -0700

    Update appraisal_app.py

--- appraisal_app.py
+++ appraisal_app.py
@@ -32,20 +32,20 @@ def display_house_data(house_data):
             st.write(house_data['property']['property_type'])
 
             st.subheader("Bedrooms")
-            st.write(house_data['property']['bedrooms'])
+            st.write(house_data['property'].get('bedrooms', 'N/A'))
 
             st.subheader("Bathrooms")
-            st.write(house_data['property']['bathrooms'])
+            st.write(house_data['property'].get('bathrooms', 'N/A'))
 
         with col2:
             st.subheader("Year Built")
             st.write(house_data['property']['year_built'])
 
             st.subheader("Living Area (sq.ft)")
-            st.write(house_data['property']['living_area'])
+            st.write(house_data['property'].get('living_area', 'N/A'))
 
             st.subheader("Lot Area (sq.ft)")
-            st.write(house_data['property']['lot_area'])
+            st.write(house_data['property'].get('lot_area', 'N/A'))
 
     except KeyError as e:
         st.error(f"Failed to display data. Missing field: {e}")
@@ -61,7 +61,6 @@ api_secret = "xVRYZEOZqBmheOYmFJsFDphFd6vFTRGL"
 if st.button("Fetch Data"):
     house_data = get_house_data(address, zipcode, api_key, api_secret)
     if house_data:
-        st.write(house_data)  # Temporary line for debugging
         display_house_data(house_data)
     else:
         st.error("Failed to fetch data. Please try again.")
