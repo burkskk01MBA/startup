fb09a4016a85f4674e3c42f8184ec774ee4067b4
burkskk01MBA <128370379+burkskk01MBA@users.noreply.github.com>
2023-03-20 06:24:47 -0700

    Update appraisal_app.py

--- appraisal_app.py
+++ appraisal_app.py
@@ -2,26 +2,30 @@ import streamlit as st
 import requests
 
 
-def get_house_data(address, api_key, api_secret):
-    url = f"https://api.housecanary.com/v2/property/details?address={address}&zipcode={zipcode}"
-    response = requests.get(url, auth=(api_key, api_secret))
+def get_house_data(address, zip_code, api_key, api_secret):
+    # Make API request to HouseCanary endpoint and return result
+    url = f"https://api.housecanary.com/v2/property/details?address={address}&zipcode={zip_code}"
+    headers = {"HouseCanary-Api-Key": api_key, "HouseCanary-Api-Secret": api_secret}
+    response = requests.get(url, headers=headers)
     return response.json()["property/details"]["result"]
 
 
-def app():
-    st.title("House Value Estimator")
-    st.write("Enter the address and zipcode of the house you want to get an estimate for:")
-    address = st.text_input("Address:")
-    zipcode = st.text_input("Zipcode:")
+# Main Streamlit app code
+st.title("HouseCanary Property Details")
 
-    if st.button("Get estimate"):
-        api_key = "test_ZHR10MHG9YVB19SPIKG9"
-        api_secret = "vDy4m18y7oNi9u3zFFXqk4fJmSV8X6GW"
+# Get user input
+address = st.text_input("Enter Address")
+zip_code = st.text_input("Enter Zip Code")
 
-        house_data = get_house_data(address, zipcode, api_key, api_secret)
-
-        st.write(f"Estimated value: ${house_data['avm']['value']}")
-
-
-if __name__ == "__main__":
-    app()
+# Button to trigger API request and show results
+if st.button("Get Property Details"):
+    # Get API credentials (you can replace with your own if you have them)
+    api_key = "test_ZHR10MHG9YVB19SPIKG9"
+    api_secret = "vDy4m18y7oNi9u3zFFXqk4fJmSV8X6GW"
+    
+    # Make API request and display results
+    try:
+        house_data = get_house_data(address, zip_code, api_key, api_secret)
+        st.write(house_data)
+    except:
+        st.write("Error: Could not retrieve property details.")
