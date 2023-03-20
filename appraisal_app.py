46a274b411e571091061135ac73fbacf7dbcbec6
burkskk01MBA <128370379+burkskk01MBA@users.noreply.github.com>
2023-03-20 05:03:22 -0700

    Update appraisal_app.py

--- appraisal_app.py
+++ appraisal_app.py
@@ -1,69 +1,34 @@
 import streamlit as st
 import requests
-import pandas as pd
 import base64
 
-def get_house_data(address, zipcode, api_key, api_secret):
-    url = f"https://api.housecanary.com/v2/property/details?address={address}&zipcode={zipcode}"
-    headers = {
-        "Authorization": "Basic " + base64.b64encode(f"{api_key}:{api_secret}".encode("ascii")).decode("ascii")
-    }
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error(f"Failed to fetch data. Status code: {response.status_code}. Response text: {response.text}")
-        return None
+# Functions for fetching data and displaying it
+# ...
 
-def display_house_data(house_data):
-    st.header("Property Details")
+def app():
+    st.title("House Canary Property Details")
 
-    try:
-        # Display address
-        st.subheader("Address")
-        property_data = house_data[0]['property/details']['result']['property']
-        address_data = house_data[0]['address_info']
-        st.markdown(f"{address_data['address']}, {address_data['city']}, {address_data['state']} {address_data['zipcode']}")
+    # Add a sidebar for user input
+    st.sidebar.header("Input Property Information")
 
-        # Display property details in columns
-        col1, col2 = st.columns(2)
+    address = st.sidebar.text_input("Address", "123 Main St")
+    zipcode = st.sidebar.text_input("Zipcode", "12345")
 
-        with col1:
-            st.subheader("Property Type")
-            st.write(property_data['property_type'])
+    api_key = "test_QXKKXWIHFL71J1D524Z1"
+    api_secret = "xVRYZEOZqBmheOYmFJsFDphFd6vFTRGL"
 
-            st.subheader("Bedrooms")
-            st.write(property_data.get('bedrooms', 'N/A'))
+    st.sidebar.button("Fetch Property Details")
 
-            st.subheader("Bathrooms")
-            st.write(property_data.get('bathrooms', 'N/A'))
-
-        with col2:
-            st.subheader("Year Built")
-            st.write(property_data['year_built'])
-
-            st.subheader("Living Area (sq.ft)")
-            st.write(property_data.get('living_area', 'N/A'))
-
-            st.subheader("Lot Area (sq.ft)")
-            st.write(property_data.get('lot_area', 'N/A'))
-
-    except KeyError as e:
-        st.error(f"Failed to display data. Missing field: {e}")
-
-
-st.title("House Canary Web App")
-
-address = st.text_input("Enter the address:")
-zipcode = st.text_input("Enter the zipcode:")
-
-api_key = "test_QXKKXWIHFL71J1D524Z1"
-api_secret = "xVRYZEOZqBmheOYmFJsFDphFd6vFTRGL"
-
-if st.button("Fetch Data"):
+    # Fetch data
     house_data = get_house_data(address, zipcode, api_key, api_secret)
+
     if house_data:
         display_house_data(house_data)
-    else:
-        st.error("Failed to fetch data. Please try again.")
+
+    # Add a footer
+    st.markdown("---")
+    st.markdown("This web app is using data from the House Canary API. For more information, please visit their [website](https://www.housecanary.com/).")
+
+if __name__ == "__main__":
+    app()
+
